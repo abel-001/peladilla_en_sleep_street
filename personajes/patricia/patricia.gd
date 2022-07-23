b1125d45ce4b5cd870e9f0015916399843d9ccb9
matildagame <55888362+matildagame@users.noreply.github.com>
2022-07-24 00:54:11 +0200

    Nuevo fondo par anivel de prueba, y scroll

--- personajes/patricia/patricia.gd
+++ personajes/patricia/patricia.gd
@@ -7,9 +7,11 @@ extends KinematicBody2D
 
 export var velocidad=100
 
+var flip=false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("walking",-1,2)
+	$AnimationPlayer.play("idle",-1,2)
 
 #
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,14 +19,24 @@ func _ready():
 #	pass
 
 func _physics_process(delta):
+	var velocidad_y=0
+	
+	if Input.is_action_pressed("ui_up"):
+		velocidad_y=-velocidad
+	elif Input.is_action_pressed("ui_down"):
+		velocidad_y=velocidad	
 	
 	if Input.is_action_pressed("ui_right"):
-		move_and_slide(Vector2(velocidad,0),Vector2(0,1))
+		move_and_slide(Vector2(velocidad,velocidad_y),Vector2(0,1))
 		$AnimationPlayer.play("walking",-1,2)
-	elif Input.is_action_just_pressed("slap"):
+		flip=false
+	elif Input.is_action_pressed("ui_left"):
+		move_and_slide(Vector2(-velocidad,velocidad_y),Vector2(0,1))
+		$AnimationPlayer.play("walking2",-1,2)
+		flip=true
+	elif Input.is_action_pressed("slap"):
 		$AnimationPlayer.play("slapping",-1,3)
 	else:
-		if $AnimationPlayer.is_playing():
-			pass
-		else:
-			$AnimationPlayer.stop()
+		$AnimationPlayer.play("idle")
+		
+	$"Walking-patriciaBis".flip_h=flip
